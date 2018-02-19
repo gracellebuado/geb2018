b852602ef42ea37b29258c263b9ac165a53b5df7
gracellebuado <36612289+gracellebuado@users.noreply.github.com>
2018-02-19 02:55:48 +0000

    Add files via upload

---
+++ buado.py
@@ -0,0 +1,252 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print "hello world"
+hello world
+>>> print "python version"
+python version
+>>> import sys
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+python version
+2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)]
+version info.
+sys.version_info(major=2, minor=7, micro=14, releaselevel='final', serial=0)
+>>> 
+>>> 
+>>> 5%0
+
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    5%0
+ZeroDivisionError: integer division or modulo by zero
+>>> 5)
+SyntaxError: invalid syntax
+>>> 5%
+SyntaxError: invalid syntax
+>>> 1+3*4
+13
+>>> 1425%27
+21
+>>> 1425/27
+52
+>>> 4%14
+4
+>>> 4/14
+0
+>>> 14/4
+3
+>>> 14%4
+2
+>>> 2.143e17
+2.143e+17
+>>> 1/2.0
+0.5
+>>> 15/2
+7
+>>> 15.0/2.0
+7.5
+>>> 10/3
+3
+>>> 10.0000/3.0000
+3.3333333333333335
+>>> 10/3.0000
+3.3333333333333335
+>>> sqrt(2)
+
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    sqrt(2)
+NameError: name 'sqrt' is not defined
+>>> sqrt(25)
+
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    sqrt(25)
+NameError: name 'sqrt' is not defined
+>>> sqrt 25
+SyntaxError: invalid syntax
+>>> e
+
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    e
+NameError: name 'e' is not defined
+>>> e
+
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    e
+NameError: name 'e' is not defined
+>>> e
+
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    e
+NameError: name 'e' is not defined
+>>> e
+
+Traceback (most recent call last):
+  File "<pyshell#28>", line 1, in <module>
+    e
+NameError: name 'e' is not defined
+>>> pi
+
+Traceback (most recent call last):
+  File "<pyshell#29>", line 1, in <module>
+    pi
+NameError: name 'pi' is not defined
+>>> 
+>>> 
+>>> from math import
+SyntaxError: invalid syntax
+>>> from math import*
+>>> pi
+3.141592653589793
+>>> e
+2.718281828459045
+>>> sqrt(25)
+5.0
+>>> abs(-200)
+200
+>>> x=2
+>>> 
+>>> x+3
+5
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+
+
+>>> 
+
+>>> 
+
+
+>>> 
+
+>>> 
+
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+
+>>> 
+
+>>> 
+
+
+>>> 
+
+>>> 
+
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+>>> x+5
+7
+>>> 7
+7
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> print "hello world"
+hello world
+>>> time=3
+>>> time=30
+>>> print "you have", 90-time, "minutes before lunch"
+you have 60 minutes before lunch
+>>> print "you have", 120-time, "minutes before lunch"
+you have 90 minutes before lunch
+>>> print "you have", 119-time, "minutes before lunch"
+you have 89 minutes before lunch
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+how hungry are you?
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+what time is it?35
+('the current time is', 35)
+('you have', 81, 'minutes before lunch')
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+what time is it?35
+('the current time is after 10', 35)
+('you have', 81, 'minutes before lunch')
+>>> 
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+what time is it?36
+('the current time is after 10', 36)
+('you have', 84, 'minutes before lunch')
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+what time is it?37
+the current time is after 10 37
+you have 83 minutes before lunch
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+what time is it?38
+the current time after 10 am is: 38
+you have 82 minutes before lunch
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+what is the current time after 10 am?44
+the current time is: 44
+your lunch will be delivered 76 minutes hold ur gutom
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+what is the current time after 10 am?45
+the current time is: 45 minutes after 10 am
+your lunch will be delivered 75 minutes hold ur gutom
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+what is the current time after 10 am?45
+the current time is 45 minutes after 10 am
+your lunch will be delivered 75 minutes hold ur gutom
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+what is the current time after 10 am? 45
+the current time is 45 minutes after 10 am
+your lunch will be delivered 75 minutes hold ur gutom hehe
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+what is the current time after 10 am?47
+the current time is 47 minutes after 10 am
+your lunch will be delivered 73 minutes hold ur gutom hehe
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+gutom ka na ba?yes
+
+Traceback (most recent call last):
+  File "//psf/Home/Desktop/1.py", line 5, in <module>
+    x=input ("gutom ka na ba?")
+  File "<string>", line 1, in <module>
+NameError: name 'yes' is not defined
+>>> 
+====================== RESTART: //psf/Home/Desktop/1.py ======================
+what is the current time after 10 am?49
+the current time is 49 minutes after 10 am
+your lunch will be delivered 71 minutes hold ur gutom hehe
+>>> 
